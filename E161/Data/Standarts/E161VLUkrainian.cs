75c9af592b5103b438822aae59be2470b86840b3
MVozniak <mikola.v.2016@gmail.com>
2023-12-14 00:59:31 +0200

    Label Fix

--- E161/Data/Standarts/E161VLUkrainian.cs
+++ E161/Data/Standarts/E161VLUkrainian.cs
@@ -3,7 +3,6 @@
     public class E161VLUkrainian : Standart
     {
         public override string Name => "E161-Code Ukrainian";
-
         public override string Details => "ETSI ES 202 130 V2.1.2 p. 7.3.74 Ukrainian";
         public override string Key2 => "абвгґ2abc";
         public override string Key3 => "деєжзё3def";
