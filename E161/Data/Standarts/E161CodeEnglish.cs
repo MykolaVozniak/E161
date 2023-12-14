05ed7d4e0f3f71289ab55fa691564e7bb9e155bd
MVozniak <mikola.v.2016@gmail.com>
2023-12-14 05:41:25 +0200

    Recommit

--- E161/Data/Standarts/E161CodeEnglish.cs
+++ E161/Data/Standarts/E161CodeEnglish.cs
@@ -1,6 +1,6 @@
 ﻿namespace E161.Data.Standarts
 {
-    public class E161CodeEnglis : Standart
+    public class E161CodeEnglish : Standart
     {
         public override string Name => "E161-Code Englis";
         public override string Details => "ETSI ES 202 130 V2.1.2 p. 7.3.19 English";
--- E161/Data/Standarts/Standart.cs
+++ E161/Data/Standarts/Standart.cs
@@ -6,7 +6,7 @@
 
         public abstract string Details { get; }
         //Language-independent signals
-        public string Signal1 => ".,?!1@'\"-()/:_;+&%*=<>€£$¥¤[]{}\\~^¿¡§#"; //ABCDEF
+        public string Signal1 => ".,?!1@'\"-()/:_;+&%*=<>€£$¥¤[]{}\\~^¿¡§#";
         public string Signal0 => " 0\n";
 
         public string SignalChars => "0123456789-()#*";
@@ -22,7 +22,9 @@
         public abstract string Signal8 { get; }
         public abstract string Signal9 { get; }
 
-        public string[] Assignments => new[] { Signal0, Signal1, Signal2, Signal3, Signal4, Signal5, Signal6, Signal7, Signal8, Signal9 };
-
+        public string[] Assignments => new[] { Signal0, 
+            Signal1, Signal2, Signal3, 
+            Signal4, Signal5, Signal6, 
+            Signal7, Signal8, Signal9 };
     }
 }
